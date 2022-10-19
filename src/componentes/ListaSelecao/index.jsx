ae11b731b0f40312398b49d30d951ec6b316d40e
Arydiane <103674349+Arydiane@users.noreply.github.com>
2022-10-19 19:47:44 -0300

    altera componente lista seleção

--- src/componentes/ListaSelecao/index.jsx
+++ src/componentes/ListaSelecao/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from './ListaSelecao.module.scss'
 
-export default function ListaSelecao({label, itens, obrigatorio=false}) {
+export default function ListaSelecao({label, valor, aoAlterado,  itens, obrigatorio=false}) {
   return (
     <div className={styles.listaSelecao}>
         <label>{label}</label>
-        <select required={obrigatorio}>
+        <select required={obrigatorio} value={valor} onChange={evento => aoAlterado(evento.target.value)}>
             {itens.map(item => <option key={item.id}>{item.nome}</option>)}
         </select>
     </div>
