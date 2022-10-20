1f61d10363e1766d740cc90927a0a755adf719b6
Arydiane <103674349+Arydiane@users.noreply.github.com>
2022-10-20 18:40:15 -0300

    estiliza o componente categoria

--- src/componentes/Categoria/index.jsx
+++ src/componentes/Categoria/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from './Categoria.module.scss'
 import CardLivro from "componentes/CardLivro";
+import hexToRgba from 'hex-to-rgba';
 
 export default function Categoria({ livros, categoria }) {
 
     return (
-        <div className={styles.categoria} style={{ background: categoria.cor }}>
-            <h3>{categoria.nome}</h3>
+        <div className={styles.categoria} style={{ background: hexToRgba(categoria.cor, '0.3') }}>
+            <h3 style={{ borderColor: categoria.cor}}>{categoria.nome}</h3>
             <div className={styles.categoria__itens}>
                 {livros.map((livro, indice) => {
                     return (
